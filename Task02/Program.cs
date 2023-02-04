05ae1053b15051435b1db93e5f009c9841c76879
AnnaEvgrafova <eaa79evgrafova@yandex.ru>
2023-02-04 15:23:04 +0500

    Урок 5

---
+++ Task02/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задача 2: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0
+*/
+
+
+int[] GenerateArray(int len)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(1, 10);   //число в массиве
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item}\t");
+    }
+    System.Console.WriteLine();
+
+}
+
+int SumSign(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((i % 2) == 0) // берем индекс
+          {
+            sum = sum + array[i];
+        }
+
+    }
+    return sum;
+}
+
+int[] array = GenerateArray(4);
+PrintArray(array);
+
+System.Console.WriteLine($"Сумма чисел на нечётных позициях = {SumSign(array)}");
+
---
+++ Task03/Program.cs
@@ -0,0 +1,58 @@
+﻿/*Задача 3: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементами массива.
+
+[3 7 22 2 78] -> 76*/
+
+int[] GenerateArray(int len = 10)
+{
+    int[] array = new int[len];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item}\t");
+    }
+    System.Console.WriteLine();
+}
+
+int[] array = GenerateArray(5);
+PrintArray(array);
+
+int minimum = array[0];
+int maximum = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < minimum)
+    {
+        minimum = array[i];
+    }
+    {
+        if (array[i] > maximum)
+        {
+            maximum = array[i];
+        }
+
+    }
+}
+
+Console.WriteLine($"Минимальное число в массве = {minimum}");
+System.Console.WriteLine($"Максимальное число в массве = {(maximum)}");
+
+int result = maximum - minimum;
+System.Console.WriteLine($"Разница между максимальным и минимальным значением = {result}");
+
+
+
+
+
+
+
